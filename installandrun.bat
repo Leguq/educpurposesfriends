5ae8289230864f4400a48e2ddb7c1240cef3a29c
Leguq <131692821+Leguq@users.noreply.github.com>
2024-11-18 18:07:33 +0100

    Update installandrun.bat

--- installandrun.bat
+++ installandrun.bat
@@ -1,4 +1,7 @@
 @echo off
+if not exist "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312\python.exe" (
+timeout 30 >nul
+)
 pip uninstall pycrypto -y
 pip install discord.py
 pip install pycryptodome
