cbaff9671a10423fe265f3e26e4c0293da0ee1c7
Leguq <131692821+Leguq@users.noreply.github.com>
2024-11-18 18:23:25 +0100

    Update installandrun.bat

--- installandrun.bat
+++ installandrun.bat
@@ -1,7 +1,50 @@
 @echo off
+
 if not exist "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312\python.exe" (
-timeout 30 >nul
+
+
+
+set SCOOP_BIN="%USERPROFILE%\scoop\shims\python.exe"
+
+:check_scoop
+:: Check if Scoop is installed
+where scoop >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Scoop is not installed. Installing Scoop...
+    powershell -Command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"
+    timeout 5 >nul
+) else (
+    echo Scoop is already installed.
 )
+
+:check_python
+:: Check if Python is installed via Scoop
+if exist "%SCOOP_BIN%" (
+    echo Python is already installed.
+    goto continue_script
+) else (
+    echo Python is not installed. Installing Python...
+    scoop install python
+    timeout 5 >nul
+)
+
+:: Wait until Python is installed
+:check_python_loop
+if not exist "%SCOOP_BIN%" (
+    timeout /t 5 >nul
+    goto check_python
+)
+
+
+
+
+)
+:continue_script
+echo Python installation verified. Continuing with the script...
+:: Add your additional script logic below
+
+
+
 pip uninstall pycrypto -y
 pip install discord.py
 pip install pycryptodome
